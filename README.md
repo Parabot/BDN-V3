141cc22cf0b46db8fad894462a49d90150b4c947
JKetelaar <jeroen@ketelaar.me>
2015-12-05 01:12:46 +0100

    [TASK] First release

---
+++ README.md
@@ -0,0 +1,4 @@
+BDN
+===
+
+A Symfony project created on October 27, 2015, 7:35 am.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Group")
+     * @ORM\JoinTable(name="user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
