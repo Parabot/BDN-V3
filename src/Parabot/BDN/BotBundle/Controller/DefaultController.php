1e4cebbf0d2b347ce000044d33cba3189511dab5
JKetelaar <jeroen@ketelaar.me>
2016-05-24 19:47:05 +0200

    [TASK] Added more basic tests

--- src/Parabot/BDN/BotBundle/Controller/DefaultController.php
+++ src/Parabot/BDN/BotBundle/Controller/DefaultController.php
@@ -2,10 +2,13 @@
 
 namespace Parabot\BDN\BotBundle\Controller;
 
+use Parabot\BDN\BotBundle\BDNBotBundle;
 use Parabot\BDN\BotBundle\Entity\Types\Client;
+use Parabot\BDN\BotBundle\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
@@ -18,6 +21,18 @@ class DefaultController extends Controller
         $client = new Client();
         $client->setVersion("2.5.1-RC-15816222");
         $client->setStable(false);
+        $client->setReleaseDate(new \DateTime());
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($client);
+        $manager->flush();
+
+        /**
+         * @var ClientRepository $repository
+         */
+        $repository = $manager->getRepository('BDNBotBundle:Types\Client');
+        var_dump($repository->findAllByStability(false));
+
         return array('name' => $type);
     }
 }
