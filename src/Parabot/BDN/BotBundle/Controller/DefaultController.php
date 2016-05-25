7cad17b96ce9e8f266463292ac7afe776de97e13
JKetelaar <jeroen@ketelaar.me>
2016-05-25 18:52:22 +0200

    [FEATURE] Finished download and added creation tests

--- src/Parabot/BDN/BotBundle/Controller/DefaultController.php
+++ src/Parabot/BDN/BotBundle/Controller/DefaultController.php
@@ -7,6 +7,9 @@ use Parabot\BDN\BotBundle\BDNBotBundle;
 use Parabot\BDN\BotBundle\Entity\Types\Client;
 use Parabot\BDN\BotBundle\Entity\Types\Type;
 use Parabot\BDN\BotBundle\Repository\ClientRepository;
+use Parabot\BDN\BotBundle\Repository\TypeRepository;
+use Parabot\BDN\BotBundle\Service\TravisHelper;
+use Parabot\BDN\BotBundle\Service\TypeHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -23,21 +26,46 @@ class DefaultController extends Controller {
      * @param Request $request
      * @param string  $type
      *
-     * @return array
+     * @return JsonResponse
      */
-    public function indexAction(Request $request, $type) {
-        $manager = $this->getDoctrine()->getManager();
+    public function downloadAction(Request $request, $type) {
+        /**
+         * @var TypeRepository $repository
+         * @var TypeHelper     $typeHelper
+         */
+        $manager    = $this->getDoctrine()->getManager();
+        $branch     = $request->query->get('branch');
+        $typeHelper = $this->get('bot.type_helper');
         $repository = null;
+        $download   = null;
 
-        switch(strtolower($type)) {
-            case 'client':
-                /** @var ClientRepository $repository */
-                $repository = $manager->getRepository('BDNBotBundle:Types\Client');
-                break;
-            default:
-                return new JsonResponse([ 'result' => 'Unknown type requested' ], 404);
+        if($typeHelper->typeExists($type)) {
+            $repository = $manager->getRepository($typeHelper->getRepositorySlug($type));
+
+            $download = $repository->findLatestByStability(
+                ! ($request->query->get('nightly') === 'true'),
+                $branch
+            );
+        } else {
+            return new JsonResponse([ 'result' => 'Unknown type requested' ], 404);
         }
 
+        if($download != null) {
+            $result = $this->get('bot.download_manager')->provideDownload($download);
+            if($result === false) {
+                return new JsonResponse(
+                    [
+                        'result'     => 'Could not find requested type file',
+                        'help'       => 'Please ask an administrator to solve this issue',
+                        'error_code' => '3H84STO8JYKB',
+                    ], 500
+                );
+            } else {
+                return $result;
+            }
+        } else {
+            return new JsonResponse([ 'result' => 'No version of type or branch found' ], 404);
+        }
 
         if($request->query->get('create') == 'true') {
             $client = new Client();
@@ -49,9 +77,7 @@ class DefaultController extends Controller {
             $manager->flush();
         }
 
-        $c = $repository->findLatestByStability($request->query->get('nightly') === 'false');
-
-        if ($request->query->get('download') === 'true') {
+        if($request->query->get('download') === 'true') {
 
             /** @var S3Client $aws */
             $aws = $this->get('aws.s3');
@@ -61,7 +87,29 @@ class DefaultController extends Controller {
             $body->rewind();
             file_put_contents($c->getPath() . 'client-2.jar', $body->read($result[ 'ContentLength' ]));
         }
+    }
 
-        return [ 'name' => $c->getType() ];
+    /**
+     * @Route("/create/{type}")
+     * @Template()
+     *
+     * @param Request $request
+     * @param Type    $type
+     *
+     * @return JsonResponse
+     */
+    public function createAction(Request $request, $type) {
+        /**
+         * @var TravisHelper $helper
+         * @var TypeHelper $typeHelper
+         */
+        $helper = $this->get('bot.travis_helper');
+        $typeHelper = $this->get('bot.type_helper');
+        $r = $request->query->get('builder_id');
+
+        if($typeHelper->typeExists($type)) {
+            return new JsonResponse($helper->getLatestBuild($type->getTravisRepository(), $r));
+        }
+        return new JsonResponse(['result' => 'Unknown type requested'], 404);
     }
 }
