19f53d8d888ea626a67e120fecec750729ef039c
JKetelaar <jeroen@ketelaar.me>
2016-05-25 19:42:40 +0200

    [FEATURE] Added ability to construct type out of string

--- src/Parabot/BDN/BotBundle/Service/TypeHelper.php
+++ src/Parabot/BDN/BotBundle/Service/TypeHelper.php
@@ -5,17 +5,35 @@
 
 namespace Parabot\BDN\BotBundle\Service;
 
+use Doctrine\ORM\EntityManager;
+use Parabot\BDN\BotBundle\Entity\Types\Type;
+
 class TypeHelper {
 
+    /**
+     * @var string[] $types
+     */
     private static $types = [
         'client' => 'BDNBotBundle:Types\Client',
     ];
 
+    private $entityManager;
+
     /**
      * TypeHelper constructor.
+     *
+     * @param EntityManager $entityManager
      */
-    public function __construct() { }
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
+    /**
+     * @param string $type
+     *
+     * @return bool
+     */
     public function typeExists($type){
         foreach(self::$types as $key => $value){
             if (strtolower($key) == strtolower($type)){
@@ -24,8 +42,24 @@ class TypeHelper {
         }
         return false;
     }
-    
+
+    /**
+     * @param string $type
+     *
+     * @return string
+     */
     public function getRepositorySlug($type){
         return self::$types[strtolower($type)];
+    }
+
+    /**
+     * @param $type
+     *
+     * @return Type
+     */
+    public function createType($type){
+        $repository = $this->getRepositorySlug($type);
+        $class = $this->entityManager->getClassMetadata($repository)->getName();
+        return new $class();
     }
 }
