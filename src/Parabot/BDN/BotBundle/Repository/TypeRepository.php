153b10eb0a1e3a1f9e02db70cb1b537fb91ac7b2
JKetelaar <jeroen@ketelaar.me>
2016-05-25 18:51:43 +0200

    [FEATURE] Added ruling Type repository interface

---
+++ src/Parabot/BDN/BotBundle/Repository/TypeRepository.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace Parabot\BDN\BotBundle\Repository;
+
+use Parabot\BDN\BotBundle\Entity\Types\Type;
+
+interface TypeRepository {
+
+    /**
+     * @param boolean $stable
+     *
+     * @return Type[]
+     */
+    public function findAllByStability($stable);
+    
+    /**
+     * @param boolean     $stable
+     *
+     * @param null|string $branch
+     *
+     * @return null|Type
+     */
+    public function findLatestByStability($stable, $branch = null);
+}
