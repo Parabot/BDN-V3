3b5fc4a821955cd4bce6130d5e93a5038d1c8c6a
JKetelaar <jeroen@ketelaar.me>
2016-05-25 14:07:26 +0200

    [TASK] Added some more basic tests

--- src/Parabot/BDN/BotBundle/Controller/DefaultController.php
+++ src/Parabot/BDN/BotBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace Parabot\BDN\BotBundle\Controller;
 
+use Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
+use Aws\Sdk;
 use Parabot\BDN\BotBundle\BDNBotBundle;
 use Parabot\BDN\BotBundle\Entity\Types\Client;
 use Parabot\BDN\BotBundle\Repository\ClientRepository;
@@ -38,7 +41,15 @@ class DefaultController extends Controller {
         $repository = $manager->getRepository('BDNBotBundle:Types\Client');
 
         $c = $repository->findLatestByStability($request->query->get('nightly') === 'false');
-        var_dump($c->getPath());
+//        var_dump($c->getPath());
+
+        /** @var S3Client $aws */
+        $aws = $this->get('aws.s3');
+
+        $result = $aws->getObject(['Bucket' => 'parabot', 'Key' => 'artifacts/Parabot-V2.5.1.jar']);
+        $body = $result->get('Body');
+        $body->rewind();
+        file_put_contents($c->getPath() . 'client-2.jar', $body->read($result['ContentLength']));
 
         return [ 'name' => $type ];
     }
