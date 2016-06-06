427074fb3ef272671f4fe4db8e49c2c1b171c2d9
JKetelaar <jeroen@ketelaar.me>
2016-06-06 23:42:32 +0200

    [FEATURE] Added Category for scripts

---
+++ src/Parabot/BDN/BotBundle/Entity/Scripts/Category.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Parabot\BDN\BotBundle\Entity\Scripts;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="scripts", type="array")
+     */
+    private $scripts;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set scripts
+     *
+     * @param array $scripts
+     * @return Category
+     */
+    public function setScripts($scripts)
+    {
+        $this->scripts = $scripts;
+
+        return $this;
+    }
+
+    /**
+     * Get scripts
+     *
+     * @return array 
+     */
+    public function getScripts()
+    {
+        return $this->scripts;
+    }
+}
