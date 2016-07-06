7c051f64fc91f1b933a95780d972bf4c94f2336f
JKetelaar <jeroen@ketelaar.me>
2016-07-06 03:21:15 +0200

    [FEATURE] Added serializer manager, to output objects as json

---
+++ src/AppBundle/Service/SerializerManager.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace AppBundle\Service;
+
+use AppBundle\Service\Normalizers\DateTimeNormalizer;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class SerializerManager {
+
+    /**
+     * @return Serializer
+     */
+    public static function getSerializers(){
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new DateTimeNormalizer(), new ObjectNormalizer($classMetadataFactory));
+
+        $serializer = new Serializer($normalizers, $encoders);
+        return $serializer;
+    }
+
+    /**
+     * @param        $object
+     * @param string $format
+     * @param array  $groups
+     *
+     * @return object
+     */
+    public static function normalize($object, $format = 'json', $groups = ['default']){
+        return SerializerManager::getSerializers()->normalize($object, $format, ['groups' => $groups]);
+    }
+}
