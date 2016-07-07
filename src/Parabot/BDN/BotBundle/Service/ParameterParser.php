07aff43e4ba23fb4a75a98066870b523ebd7a4e9
JKetelaar <jeroen@ketelaar.me>
2016-07-07 19:16:14 +0200

    [FEATURE] Added class to parse parameters

---
+++ src/Parabot/BDN/BotBundle/Service/ParameterParser.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace Parabot\BDN\BotBundle\Service;
+
+class ParameterParser {
+
+    /**
+     * @param string $request
+     *
+     * @return bool
+     */
+    public static function parseStringToBoolean($request){
+        if (!is_numeric($request)) {
+            return $request == 'true' ? true : false;
+        }else{
+            return $request == 1 ? true : false;
+        }
+    }
+}
