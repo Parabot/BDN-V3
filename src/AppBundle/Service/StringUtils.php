1fea2338b5354e533b3d9804fc3366fe61f2af93
JKetelaar <jeroen@ketelaar.me>
2016-07-27 02:35:47 +0200

    [FEATURE] Added the ability to hide the special characters

--- src/AppBundle/Service/StringUtils.php
+++ src/AppBundle/Service/StringUtils.php
@@ -6,8 +6,8 @@
 namespace AppBundle\Service;
 
 class StringUtils {
-    public static function generateRandomString($length = 15) {
-        $characters       = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?<>,.;:';
+    public static function generateRandomString($length = 15, $specials = true) {
+        $characters       = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' . ($specials === true ? '!@#$%^&*()?<>,.;:' : '');
         $charactersLength = strlen($characters);
         $randomString     = '';
         for($i = 0; $i < $length; $i++) {
