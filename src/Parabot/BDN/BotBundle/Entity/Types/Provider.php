077ac93f934425d783321e9d2c74775f8e47a94d
JKetelaar <jeroen@ketelaar.me>
2016-10-23 00:28:26 +0200

    [FEATURE] Added Provider type

---
+++ src/Parabot/BDN/BotBundle/Entity/Types/Provider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace Parabot\BDN\BotBundle\Entity\Types;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Provider
+ *
+ * @ORM\Table(name="type_provider")
+ * @ORM\Entity(repositoryClass="Parabot\BDN\BotBundle\Repository\ClientRepository")
+ */
+class Provider extends Type {
+
+    /**
+     * Client constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getType() {
+        return 'Provider';
+    }
+
+    /**
+     * @return string
+     */
+    public function getTravisRepository() {
+        return 'Parabot/Parabot-317-API-Minified';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'Provider';
+    }
+}
