e04fde1bbd62dd8779f36dc50839edb2b33afa92
JKetelaar <jeroen@ketelaar.me>
2017-08-18 13:50:49 +0200

    [FEATURE] Added Provider support for DreamScape and PkHonor

---
+++ src/Parabot/BDN/BotBundle/Entity/Types/Providers/PkHonorProvider.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author JKetelaar
+ */
+
+namespace Parabot\BDN\BotBundle\Entity\Types\Providers;
+
+/**
+ * Class PkHonorProvider
+ *
+ * @ORM\Table(name="type_pkhonor_provider")
+ * @ORM\Entity(repositoryClass="Parabot\BDN\BotBundle\Repository\ProviderRepository")
+ */
+class PkHonorProvider extends Provider {
+    public function getType() {
+        return 'PkHonor-Provider';
+    }
+
+    public function getTravisRepository() {
+        return 'Parabot/Parabot-317-API-Minified-PkHonor';
+    }
+
+    public function getName() {
+        return 'PkHonor-Provider';
+    }
+}
